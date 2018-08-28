c674aec2dc9d056b362122cfe7147f6deb7c76b6
张家瑞 <zhangjiarui.7@bytedance.com>
2018-08-28 19:44:22 +0800

    oh
    Change-Id: If344a077377d643483fe3e9023a7d1a8e6148405

--- vim_config/install_vimrc.sh
+++ vim_config/install_vimrc.sh
@@ -5,19 +5,19 @@ mkdir -p ~/.vim/bundle/VundleVim 2> /dev/null
 
 ln -si ~/.myConfig/vim_config/vimrc ~/.vim/vimrc
 ln -si ~/.myConfig/vim_config/gvimrc ~/.vim/gvimrc
-cp -R ~/.myConfig/vim_config/Vundle.vim ~/.vim/bundle/Vundle.vim
 
-#vim +BundleInstall +qall
 #echo $?
+sudo apt install git build-essential cmake python-dev python3-dev
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +BundleInstall +qall
 
 #YouCompleteMe
-#sudo apt install build-essential cmake python-dev python3-dev
-#if [ -d ~/.vim/bundle/YouCompleteMe ]; then
-#    ./install.py --go-completer # [--clang-completer --system-libclang]
-#else
-#    echo "YouCompleteMe Fail"
-#fi
+if [ -d ~/.vim/bundle/YouCompleteMe ]; then
+    ./install.py --go-completer # [--clang-completer --system-libclang]
+else
+    echo "YouCompleteMe Fail"
+fi
 
 #vim-go
-#vim +GoInstallBinaries
+vim +GoInstallBinaries
 
