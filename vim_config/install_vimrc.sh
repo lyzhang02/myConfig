b8b66dcb6aaec9cae0e12e6f0f819c49ae249c2f
张家瑞 <zhangjiarui.7@bytedance.com>
2018-08-28 20:11:50 +0800

    c

--- vim_config/install_vimrc.sh
+++ vim_config/install_vimrc.sh
@@ -7,7 +7,7 @@ ln -si ~/.myConfig/vim_config/vimrc ~/.vim/vimrc
 ln -si ~/.myConfig/vim_config/gvimrc ~/.vim/gvimrc
 
 #echo $?
-sudo apt install git build-essential cmake python-dev python3-dev
+#sudo apt install git build-essential cmake python-dev python3-dev
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +BundleInstall +qall
 
