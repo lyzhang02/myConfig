514e7476293ac897ab92d09c3c64ea2900cc63f3
张家瑞 <zhangjiarui.7@bytedance.com>
2018-08-28 19:49:35 +0800

    a
    Change-Id: I44ccf54ee7307a761d5b515ef2679e6d240b7ce3

--- vim_config/install_vimrc.sh
+++ vim_config/install_vimrc.sh
@@ -9,6 +9,7 @@ ln -si ~/.myConfig/vim_config/gvimrc ~/.vim/gvimrc
 #echo $?
 sudo apt install git build-essential cmake python-dev python3-dev
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
 vim +BundleInstall +qall
 
 #YouCompleteMe
