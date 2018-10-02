f95fc100273ee8596359cfea91c5ebb24fb25794
lyzhang02.Debian <lyzhang02@outlook.com>
2018-10-02 23:15:04 +0800

    change plugin manager to vim-plug and update ycm_extra_conf.py

--- vim_config/install_vimrc.sh
+++ vim_config/install_vimrc.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-mkdir ~/.vim 2> /dev/null
-mkdir -p ~/.vim/bundle/VundleVim 2> /dev/null
+#mkdir ~/.vim 2> /dev/null
+mkdir -p ~/.vim/autoload 2> /dev/null
+mkdir -p ~/.vim/plugged 2> /dev/null
 
 ln -si ~/.myConfig/vim_config/vimrc ~/.vim/vimrc
 ln -si ~/.myConfig/vim_config/gvimrc ~/.vim/gvimrc
 ln -si ~/.myConfig/vim_config/ycm_extra_conf.py ~/.vim/ycm_extra_conf.py
 
 #echo $?
-#sudo apt install git build-essential cmake python-dev python3-dev
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
-vim +BundleInstall +qall
+sudo apt install git build-essential cmake python-dev python3-dev
+#git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+git clone https://github.com/junegunn/vim-plug.git ~/.vim/plugged/vim-plug
+ln -sf ~/.vim/plugged/vim-plug/plug.vim ~/.vim/autoload/plug.vim
+
+#git clone --recursive https://github.com/Valloric/YouCompleteMe.git ~/.vim/plugged/YouCompleteMe
+vim +PlugInstall +qall
 
 #YouCompleteMe
 if [ -d ~/.vim/bundle/YouCompleteMe ]; then
-    cd ~/.vim/bundle/YouCompleteMe ./install.py --go-completer # [--clang-completer --system-libclang]
+    cd ~/.vim/bundle/YouCompleteMe
+    python3 install.py --go-completer --clang-completer #--system-libclang
 else
     echo "YouCompleteMe Fail"
 fi
