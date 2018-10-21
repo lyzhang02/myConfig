9bdc731283fe31ce2b90fea39ce6e1d61ca673b5
lyzhang <lyzhang@debian.lan>
2018-10-21 23:52:30 +0800

    add bash-it

--- bash_config/install.sh
+++ bash_config/install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 sudo apt install git tmux
 
-ln -fs ~/.myConfig/tmux_config/tmux_conf ~/.tmux.conf
+ln -fs ~/.myConfig/bash_config/tmux_conf ~/.tmux.conf
 #tmux source-file ~/.tmux.conf
-git clone https://github.com/Bash-it/bash-it.git ~/.bash-it
+#git clone https://github.com/Bash-it/bash-it.git ~/.bash-it
 sudo apt install autojump
-bash ~/.bash-it/install.sh
+sudo apt install bash-completion
+bash ~/.myConfig/bash_config/bash_plugins/bash-it/install.sh
 source ~/.bashrc
 bash-it enable plugins autojump
 bash-it enable completion git
