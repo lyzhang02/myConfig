c72d8c5caaef84360a6d98498fda857264df193f
lyzhang <lyzhang02@outlook.com>
2018-10-21 16:12:12 +0800

    uppdate install_vimrc

--- vim_config/install_vimrc.sh
+++ vim_config/install_vimrc.sh
@@ -6,20 +6,25 @@ mkdir -p ~/.vim/plugged 2> /dev/null
 
 ln -si ~/.myConfig/vim_config/vimrc ~/.vim/vimrc
 ln -si ~/.myConfig/vim_config/gvimrc ~/.vim/gvimrc
-ln -si ~/.myConfig/vim_config/ycm_extra_conf.py ~/.vim/ycm_extra_conf.py
+cp ~/.myConfig/vim_config/ycm_extra_conf.py ~/.vim/ycm_extra_conf.py
 
 #echo $?
 sudo apt install git build-essential cmake python-dev python3-dev
 #git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone https://github.com/junegunn/vim-plug.git ~/.vim/plugged/vim-plug
+if [ ! -d ~/.vim/plugged/vim-plug ];
+then
+    git clone https://github.com/junegunn/vim-plug.git ~/.vim/plugged/vim-plug
+else
+    echo "vim-plug has been exist"
+fi
 ln -sf ~/.vim/plugged/vim-plug/plug.vim ~/.vim/autoload/plug.vim
 
 #git clone --recursive https://github.com/Valloric/YouCompleteMe.git ~/.vim/plugged/YouCompleteMe
 vim +PlugInstall +qall
 
 #YouCompleteMe
-if [ -d ~/.vim/bundle/YouCompleteMe ]; then
-    cd ~/.vim/bundle/YouCompleteMe
+if [ -d ~/.vim/plugged/YouCompleteMe ]; then
+    cd ~/.vim/plugged/YouCompleteMe
     python3 install.py --go-completer --clang-completer #--system-libclang
 else
     echo "YouCompleteMe Fail"
