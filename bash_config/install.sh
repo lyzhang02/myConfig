5eee7d5e85d61efc86faf7f6fbd8a93fbb253d0c
lyzhang <lyzhang02@outlook.com>
2018-10-21 12:53:10 +0800

    update

--- bash_config/install.sh
+++ bash_config/install.sh
@@ -5,6 +5,8 @@ ln -fs ~/.myConfig/tmux_config/tmux_conf ~/.tmux.conf
 #tmux source-file ~/.tmux.conf
 git clone https://github.com/Bash-it/bash-it.git ~/.bash-it
 sudo apt install autojump
+bash ~/.bash-it/install.sh
 source ~/.bashrc
 bash-it enable plugins autojump
+bash-it enable completion git
 
