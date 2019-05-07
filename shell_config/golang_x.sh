6b8ee32ee6ff7a1a1f42ef25382511ce57ba17d6
zhangjiarui.7 <zhangjiarui.7@bytedance.com>
2019-05-07 11:14:01 +0800

    go x

---
+++ shell_config/golang_x.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+# Install src on golang.org from github mirror and install binaries vim-go need
+
+if [ ! $GOPATH ]
+then
+    echo "VARIABLE GOPATH NEED BE SET"
+    exit 1
+fi
+
+GOLANGX=$GOPATH/src/golang.org/x
+
+if [ ! -d $GOLANGX/tools ]
+then
+    git clone https://github.com/golang/tools.git $GOLANGX/tools
+else
+    cd $GOLANGX/tools && git pull -r
+fi
+
+cd $GOLANGX/tools/cmd/guru && go install
+echo "install goru done"
+
+cd $GOLANGX/tools/cmd/gorename && go install
+echo "install gorename done"
+
+cd $GOLANGX/tools/cmd/goimports && go install
+echo "install goimports"
+
+if [ ! -d $GOLANGX/net ]
+then
+    git clone https://github.com/golang/net.git $GOLANGX/net
+else
+    cd $GOLANGX/net && git pull -r
+fi
+
+if [ ! -d $GOLANGX/lint ]
+then
+    git clone https://github.com/golang/lint.git $GOLANGX/lint
+    cd $GOLANGX/lint/golint && go install && echo "install golint"
+else
+    cd $GOLANGX/lint && git pull -r
+fi
+
+if [ ! -d $GOLANGX/sys ]
+then
+    git clone https://github.com/golang/sys.git $GOLANGX/sys
+else
+    cd $GOLANGX/sys && git pull -r
+fi
--- vim_config/golang.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Install src on golang.org from github mirror and install binaries vim-go need
-git clone https://github.com/golang/tools.git $GOPATH/src/golang.org/x/tools
-cd $GOPATH/src/golang.org/x/tools/cmd/guru && go install && echo "install goru"
-cd $GOPATH/src/golang.org/x/tools/cmd/gorename && go install && echo "install gorename"
-cd $GOPATH/src/golang.org/x/tools/cmd/goimports && go install && echo "install goimports"
-git clone https://github.com/golang/net.git $GOPATH/src/golang.org/x/net
-git clone https://github.com/golang/lint.git $GOPATH/src/golang.org/x/lint
-cd $GOPATH/src/golang.org/x/lint/golint && go install && echo "install golint"
